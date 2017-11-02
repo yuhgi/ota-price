1f9ec2546e32ba7dee2e3cfc82a18e8a9e4d1511
yuhgi <yuhgi777@163.com>
2017-11-02 17:57:35 +0800

    地区选择组件-滑动功能

--- price-rn/src/components/common/AreaSelect/index.js
+++ price-rn/src/components/common/AreaSelect/index.js
@@ -4,7 +4,7 @@ import {View,Text,ScrollView,TouchableOpacity,Button} from 'react-native';
 import AreaSelectStyle from './style';
 
 const TYPES = [
-    '当前','热门',
+    'current','hot',
     'A','B','C','D','E','F','G',
     'H','J','K','L','M','N',
     'P','Q','R','S','T','W','X','Y','Z'
@@ -13,12 +13,30 @@ const TYPES = [
 class AreaSelect extends React.Component{
     constructor(props){
         super(props);
+        this.state = {position:{}};
     }
     _onTypePress(type){
         console.log(type);
+        
+        if(!this.state.position[type]){
+            return;
+        }
+        console.log(this.state.position[type])
+        this.refs.scrollView.scrollTo({
+            x:0,
+            y:this.state.position[type].y,
+            animated:false
+        });
     }
     _onCityPress(areaCode,name){
         this.onSelect && this.onSelect(areaCode,name);
+    }
+    _onTypeLayout(type,e){
+        this.state.position[type] = {
+            x:e.nativeEvent.layout.x,
+            y:e.nativeEvent.layout.y
+        };
+        console.log(type,this.state.position[type]);
     }
     _renderTypes(){
         return (
@@ -26,8 +44,8 @@ class AreaSelect extends React.Component{
                 {TYPES.map((type,index) => {
                     return (
                         <TouchableOpacity key={index} onPress={this._onTypePress.bind(this,type)}>
-                            <Text style={AreaSelectStyle.areaTypeItem}>{type}</Text>
-                        </TouchableOpacity> 
+                            <Text style={AreaSelectStyle.areaTypeItem}>{type==='hot'?'热门':(type==='current'?'当前':type)}</Text>
+                        </TouchableOpacity>
                     );
                 })}
             </View>
@@ -35,7 +53,7 @@ class AreaSelect extends React.Component{
     }
     _renderHot(cities){
         return (
-            <View style={AreaSelectStyle.hot}>
+            <View key="hot" style={AreaSelectStyle.hot}  onLayout={this._onTypeLayout.bind(this,'hot')}>
                 <Text style={AreaSelectStyle.cityHeader}>热门</Text>
                 <View style={AreaSelectStyle.hotCities}>
                     {cities.map((city,index) => {
@@ -56,12 +74,12 @@ class AreaSelect extends React.Component{
         let types = Object.keys(areaData);
         
         return (
-            <ScrollView>
+            <ScrollView ref="scrollView">
                 {
                     types.map((type,index) => {
                         return type === 'hot'?this._renderHot(areaData[type]):(
-                            <View key={index}>
-                                <Text style={AreaSelectStyle.cityHeader}>{type}</Text>
+                            <View key={index} onLayout={this._onTypeLayout.bind(this,type)}>
+                                <Text key={index} style={AreaSelectStyle.cityHeader}>{type}</Text>
                                 {areaData[type].map((city,index) => {
                                     return (
                                         <TouchableOpacity key={index} onPress={this._onCityPress(city.areaCode,city.name)}>
